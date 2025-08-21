acdc59d5736a4c41d5d1b4f2affe3c93abe73776
Nimsara Chamindu <nimsarachamidhu@gmail.com>
2025-08-22 01:32:04 +0530

    Add form functionality

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-import express from 'express';
-import axios from 'axios';
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
+let type = "random";
 
 app.get("/", async (req, res) => {
-    const response = await axios.get('https://official-joke-api.appspot.com/jokes/random');
-    res.render("index.ejs", {
-        question: response.data.setup,
-        answer: response.data.punchline
-    });
 
+    let response;
+
+    if (type == "random") {
+        response = await axios.get(
+            "https://official-joke-api.appspot.com/jokes/random"
+        );
+        response = response.data;
+    }
+    else{
+        response = await axios.get(
+            "https://official-joke-api.appspot.com/jokes/" + type + "/random"
+        );
+        response = response.data[0];
+    }
+
+    console.log("type = " + type);
+    console.log("RESPONSE: ");
+    console.log(response.data);
+
+    res.render("index.ejs", {
+        question: response.setup,
+        answer: response.punchline,
+    });
 });
 
-app.listen(port, ()=> {
+app.post("/", async (req, res) => {
+    
+    type = req.body.category;
+
+    console.log("POST category: " + req.body.category);
+
+    res.redirect("/");
+});
+
+app.listen(port, () => {
     console.log(`Server is Listening on port ${port}`);
 });
